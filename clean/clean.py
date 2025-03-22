a3abbab0d9f23c31f7f15b6e60487f61e537884b
Denis Musinguzi <denismusinguzi2511@gmail.com>
2025-03-23 00:11:56 +0100

    Added functionality to clean the X-ray images given the path to the image and the bounding boxes

---
+++ clean/clean.py
@@ -0,0 +1,3 @@
+# The file imeplements functionality to clean PII from X-ray images given the
+# path to the image and the bounding box coordinates of the PII.
+# The function clean_pii_from_image takes the following arguments:
